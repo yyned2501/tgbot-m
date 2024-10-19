d6f6d551545279d40431a6d1efa2550b5fda51b6
DonneChang <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>
2024-10-19 17:45:30 +0800

    更新

--- app/scripts/zhuque/ydx.py
+++ app/scripts/zhuque/ydx.py
@@ -14,19 +14,23 @@ bet_bouns = 0
 high_times = 0
 low_times = 0
 bet_point = 0
-bet_switch= 0
+bet_switch = 0
+bet_mode = 'A'
+kp_switch = 0
 rele_betbouns = 0
 lose_times =  0
 win_times = 0
 sum_losebouns = 0
+add_bet_times = 0
+last_bet_point = '小'
+last_flag = 's'
 
 @app.on_message(filters.chat(TARGET) 
-                & (filters.command("zqydx") 
-                   | filters.command("stb"))               
+                & filters.command("zqydx")             
                 &filters.me
                 )
 async def zhuque_ydx_switch(client: Client, message: Message): 
-    global bet_switch,start_bouns
+    global kp_switch,bet_switch,bet_mode,start_bouns
     if message.command[0] == "zqydx":
         if message.command[1] == "on":
             bet_switch = 1
@@ -34,16 +38,57 @@ async def zhuque_ydx_switch(client: Client, message: Message):
             await asyncio.sleep(5)
             await message.delete()
 
-        else:
+        elif message.command[1] == "on":
             bet_switch = 0
             await message.edit(f"朱雀自动 “运动鞋” 脱掉！脱掉！。。。")
             await asyncio.sleep(5)
             await message.delete()
-    elif message.command[0] == "stb":
-        if message.command[1].isdigit():
-            if (int(message.command[1]) < 100000
-                and int(message.command[1]) >= 500):
-                start_bouns = int(message.command[1])
+
+        elif message.command[1] == "set":
+            if len(message.command) >= 3:
+                if message.command[2].isdigit():
+                    if (int(message.command[1]) < 100000
+                        and int(message.command[1]) >= 500):
+                        start_bouns = int(message.command[1])
+                        await message.edit(f"底注 {start_bouns} 设置成功！。。。")
+                        await asyncio.sleep(5)
+                        await message.delete()
+
+        elif message.command[1] == "kp":
+            if len(message.command) >= 3:
+                if message.command[2] == 'on':
+                    kp_switch = 1
+                    await message.edit(f"自动开盘启动！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                elif message.command[2] == 'on':
+                    kp_switch = 0
+                    await message.edit(f"自动开盘关闭！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
+        elif message.command[1] == "md":
+
+            if len(message.command) >= 3:
+                if message.command[2] == 'a':
+                    bet_mode = 'A'
+                    await message.edit(f"mode A 启动！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                elif message.command[2] == 'b':
+                    bet_mode = 'B'
+                    await message.edit(f"mode A 启动！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                elif message.command[2] == 'c':
+                    bet_mode = 'C'
+                    await message.edit(f"mode c 启动！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
+                elif message.command[2] == 'd':
+                    bet_mode = 'D'
+                    await message.edit(f"mode c 启动！！！！。。。")
+                    await asyncio.sleep(5)
+                    await message.delete()
 
 
 @app.on_message(
@@ -54,7 +99,7 @@ async def zhuque_ydx_switch(client: Client, message: Message):
 async def zhuque_ydx_check(client: Client, message: Message):
     match = message.matches[0]
     Lottery_Point = match.group(1)
-    global bet_bouns, high_times, low_times, bet_point, bet_switch,rele_betbouns,lose_times,win_times,sum_losebouns
+    global bet_bouns, high_times, low_times, bet_point, bet_switch,bet_mode,rele_betbouns,my_username,lose_times,win_times,sum_losebouns,add_bet_times,kp_switch
       
     
     if bet_switch == 1:
@@ -64,6 +109,7 @@ async def zhuque_ydx_check(client: Client, message: Message):
                 bet_bouns = start_bouns
                 thisround_winbouns = rele_betbouns * 0.99 - sum_losebouns
                 sum_losebouns = 0
+                add_bet_times = lose_times + 1
                 win_times += 1
                 lose_times = 0
             else:
@@ -71,6 +117,7 @@ async def zhuque_ydx_check(client: Client, message: Message):
                 sum_losebouns += rele_betbouns
                 lose_times += 1
                 win_times = 0
+                add_bet_times = 0
 
         if Lottery_Point == "大":
             high_times += 1
@@ -80,23 +127,23 @@ async def zhuque_ydx_check(client: Client, message: Message):
             low_times += 1
         re_message = None
         if high_times >= 1:
-            re_message = f"连续{high_times}次大"
+            re_message = f"庄盘已连续开 “大” {high_times} 次"
         elif low_times >= 1:
-            re_message = f"连续{low_times}次小"        
+            re_message = f"庄盘已连续开 “小” {low_times} 次"  
 
         win_check = await listofWinners_check(message, setting['tg']['username'])
         if win_check:
-            re_mess= f"**[胜]** 本次实际下注 {rele_betbouns} , 本次投注盈利: {rele_betbouns * 0.99}, 连续判胜: {win_times} 次, 本轮追投盈利: {thisround_winbouns} , [{re_message}]"
+            re_mess= f"**[胜]** 本次实际下注 {rele_betbouns} , 本次投注盈利: {rele_betbouns * 0.99}, **连续判胜: {win_times} 次**, 本轮追投盈利: {thisround_winbouns} ,**[本轮共计追投 {add_bet_times} 次]** , **[{re_message}]**"
             logger.info(re_mess)            
             rele_betbouns = 0
             await app.send_message(setting['GB_VAR']['GROUP_ID']['PRIVATE_ID'],re_mess)
         else:
-            re_mess = f"**[负]** 本次实际下注 {rele_betbouns} , 本次投注亏损: {rele_betbouns} , 连续判负: {lose_times} 次, 本轮追投累计亏损 {sum_losebouns} , [{re_message}]"
+            re_mess = f"**[负]** 本次实际下注 {rele_betbouns} , 本次投注亏损: {rele_betbouns} , **连续判负: {lose_times} 次**, 本轮追投累计亏损 {sum_losebouns} , **[{re_message}]**"
             logger.info(re_mess)
             rele_betbouns = 0
             await app.send_message(setting['GB_VAR']['GROUP_ID']['PRIVATE_ID'],re_mess)
-
-        await app.send_message(TARGET,f"/ydx")    
+        if kp_switch == 1:
+            await app.send_message(TARGET,f"/ydx")    
 
 
 
@@ -106,14 +153,43 @@ async def zhuque_ydx_check(client: Client, message: Message):
 )
 async def zhuque_ydx_bet(client: Client, message: Message):
 
-    global bet_point,bet_bouns,bet_switch,rele_betbouns
+    global bet_point,bet_bouns,bet_switch,bet_mode,rele_betbouns,flag,last_bet_point,last_flag
+    
+    
     if bet_switch == 1:
-        if random.random() <= 0.5:
+        if bet_mode == 'A':   #追大
             bet_point="大"
             flag = "b"
-        else:
+
+        elif bet_mode == 'B': #追小
             bet_point="小"
             flag = "s"
+
+
+        elif bet_mode == 'C': #启动时追小，连败3败后追上次胜局，连败后3次后继续切上次胜的
+            if lose_times > 2:
+                if flag == "s":
+                    bet_point="大"
+                    flag = "b"
+                    
+                else:
+                    bet_point="小"
+                    flag = "s"
+
+            else:
+                bet_point = last_bet_point
+                flag = last_flag
+
+        elif bet_mode == 'D':
+            if random.random() <= 0.5:
+                bet_point="大"
+                flag = "b"
+            else:
+                bet_point="小"
+                flag = "s"
+
+        last_bet_point = bet_point
+        last_flag = flag
         # 计算下注金额
         if bet_bouns == 0:
             bet_bouns = start_bouns
