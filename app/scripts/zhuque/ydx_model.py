5ce13f3d64e876b22fb6c28123575b7b1f80b5c2
yyned2501 <43900325+yyned2501@users.noreply.github.com>
2024-11-07 16:47:30 +0800

    Update ydx_model.py

--- app/scripts/zhuque/ydx_model.py
+++ app/scripts/zhuque/ydx_model.py
@@ -184,6 +184,27 @@ async def zhuque_ydx_bet(client: Client, message: Message):
                         db.dx = 1 - dx.dx
                     elif db.lose_times > 0:
                         db.dx = dx.dx
+                elif db.bet_mode == "EAAA":
+                    # 仅A2模式 n1=9, n2=12, subcat2
+                    # 小类别2
+                    # prediction = 1 if sum_result == 1 else 0
+                    
+                    result9 = await session.execute(
+                        select(YdxHistory).order_by(desc(YdxHistory.id)).limit(9)
+                    )
+                    dx9 = result9.scalars().all()[-1]
+
+                    result12 = await session.execute(
+                        select(YdxHistory).order_by(desc(YdxHistory.id)).limit(12)
+                    )
+                    dx12 = result12.scalars().all()[-1]   
+                    
+                    dxsum = dx9.dx + dx12.dx
+                    dxpres = 0
+                    if dxsum == 1:
+                        dxpres = 1
+                    
+                    db.dx = dxpres
 
                 # 计算下注金额
                 remaining_bouns = int(db.sum_losebonus / rate) + db.start_bonus * (
