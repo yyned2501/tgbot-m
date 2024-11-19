175815aeeb347b38acaa5f15983f18bf61bfa8e4
杨旸 <yyned2501@vip.qq.com>
2024-11-19 15:10:45 +0800

    添加S模式

--- app/scripts/zhuque/ex/bet_modes.py
+++ app/scripts/zhuque/ex/bet_modes.py
@@ -84,6 +84,20 @@ def YD(db: ZqYdx, history: list[YdxHistory]):
     return Y(db, history, "app/onnxes/zqydx_1731937279_8_1_5115.onnx")
 
 
+def S(db: ZqYdx, history: list[YdxHistory], onnx_file):
+    model_dx = [1, 0, history[0].dx, history[9].dx, 1 - history[9].dx]
+    model_onnx = core.read_model(model=onnx_file)
+    compiled_model_onnx = core.compile_model(model=model_onnx, device_name="AUTO")
+    data = [ydx_history.dx for ydx_history in history]
+    data.reverse()
+    dummy_input = np.array(data, dtype=np.float32)
+    res = compiled_model_onnx(dummy_input)
+    output_data = res[0]
+    ov_index = np.argmax(output_data, axis=0)
+    logger.info(f"选择模式{ov_index}")
+    db.dx = model_dx[ov_index]
+
+
 def mode(func_name, *args, **kwargs):
     func = _function_registry.get(func_name)
     if callable(func):
@@ -91,6 +105,27 @@ def mode(func_name, *args, **kwargs):
     else:
         logger.error(f"不存在模式 {func_name} ,默认使用模式C")
         return mode("C", *args, **kwargs)
+
+
+@register_function("SA")
+def SA(db: ZqYdx, history: list[YdxHistory]):
+    return Y(db, history, "app/onnxes/zqydx_s_1731987416_7_2_4995.onnx")
+
+
+@register_function("SB")
+def SB(db: ZqYdx, history: list[YdxHistory]):
+    return Y(db, history, "app/onnxes/zqydx_s_1731990097_7_2_5000.onnx")
+
+
+@register_function("SC")
+def SC(db: ZqYdx, history: list[YdxHistory]):
+    return Y(db, history, "app/onnxes/zqydx_s_1731993071_7_1_4995.onnx")
+
+
+@register_function("SD")
+def SD(db: ZqYdx, history: list[YdxHistory]):
+    return Y(db, history, "app/onnxes/zqydx_s_1731999972_6_10_4950.onnx")
+
 
 def test(db: ZqYdx, history: list[YdxHistory]):
     loss_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
