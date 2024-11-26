1340a71c635c0eada50978740c7b09cc67f12c3f
杨旸 <yyned2501@vip.qq.com>
2024-11-26 15:30:03 +0800

    bug fix

--- app/scripts/zhuque/ydx_model.py
+++ app/scripts/zhuque/ydx_model.py
@@ -153,7 +153,7 @@ async def zhuque_ydx_switch(client: Client, message: Message):
                         history = history_result.scalars().all()
                         data = [ydx_history.dx for ydx_history in history]
                         models = test(db, data)
-                        r = "共有以下模型：\n```"
+                        r = "```共有以下模型：\n"
                         for k in models:
                             r += f"模型{k}：\n历史失败次数:{models[k]["loss_count"]}\n最大失败轮次:{models[k]["max_nonzero_index"]}\n胜率:{models[k]["win_rate"]:.02%}\n\n"
                         r += "```"
