0ebe37e1a314898672c18e0048bf0da2a27c5e97
杨旸 <yyned2501@vip.qq.com>
2024-12-05 10:37:15 +0800

    bug fix

--- app/scripts/zhuque/ydx_multi.py
+++ app/scripts/zhuque/ydx_multi.py
@@ -117,7 +117,9 @@ async def check_ydx_message(message: Message, base: ZqYdxBase):
         if base.message_id != message.reply_to_message_id:
             logger.warning("结算id与记录不一致，重置历史记录")
             await session.execute(
-                update(ZqYdxMulti).values(bet_bonue=0, lose_times=0, sum_losebonus=0)
+                update(ZqYdxMulti).values(
+                    bet_bonus=0, winning_streak=0, losing_streak=0, sum_losebonus=0
+                )
             )
 
 
