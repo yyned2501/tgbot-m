ccd0267dfc81a931f971d4d9356e8c607447b9ca
杨旸 <yyned2501@vip.qq.com>
2024-12-14 18:23:13 +0800

    bug  fix

--- app/schedulers/__init__.py
+++ app/schedulers/__init__.py
@@ -2,6 +2,6 @@ import importlib
 
 from app.config import launch
 
-if launch.get(__name__.split(".")) is not None:
+if launch.get(__name__.split(".")[-1]):
     for sites in launch.get(__name__.split(".")[-1]):
         importlib.import_module(f"{__name__}.{sites}")
