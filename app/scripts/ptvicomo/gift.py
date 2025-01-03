f3fa67531b8007d230af78de0c386b67e14e6148
Yy <yyned2501@vip.qq.com>
2025-01-03 16:51:29 +0800

    bug fix

--- app/scripts/ptvicomo/gift.py
+++ app/scripts/ptvicomo/gift.py
@@ -63,11 +63,12 @@ async def gift(client: Client, message: Message):
 
 
 @app.on_message(
-    filters.chat(TARGET) & custom_filters.ptvicomo_bot & filters.regex(r"奖池金额"),
+    filters.chat(
+        TARGET) & custom_filters.ptvicomo_bot & filters.regex(r"奖池金额"),
 )
 async def lottery(client: Client, message: Message):
     numbers_all = list(range(10))
     for _ in range(3):
         number = sample(numbers_all, 3)
         bonus = randint(10000, 100000)
-        await message.reply(f"{"".join(number)}*{bonus}")
+        await message.reply(f"{"".join(str(number))}*{bonus}")
