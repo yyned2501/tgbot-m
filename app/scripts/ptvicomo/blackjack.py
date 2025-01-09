96679b603160918f5e5971bbeb475b1001d279f6
yyned2501 <yyned2501@vip.qq.com>
2025-01-09 22:16:23 +0800

    test

--- app/scripts/ptvicomo/blackjack.py
+++ app/scripts/ptvicomo/blackjack.py
@@ -5,9 +5,13 @@ from pyrogram.types import Message
 from pyrogram import filters
 from app.filters import custom_filters
 from app import app
+import logging
+
+logger = logging.getLogger("main")
 
 GROUP = -1002022762746
 BOT = 7124396542
+
 
 def card_value(card):
     if card[0] in "JQK":
@@ -16,17 +20,27 @@ def card_value(card):
         return 1
     else:
         return int(card[0])
+
+
 def calculate_score(cards):
     score = sum(card_value(card) for card in cards)
     if score <= 11 and "A" in cards:
         score += 10
     return score
 
-@app.on_message(filters.chat(BOT)&filters.regex(r"庄：\?\?\? ((?:[2-9JQKA][♦♣♥♠]\s*)+)\n你(\d+)点：((?:[2-9JQKA][♦♣♥♠]\s*)+)"))
+
+@app.on_message(
+    filters.chat(BOT)
+    & filters.regex(
+        r"庄：\?\?\? ((?:[2-9JQKA][♦♣♥♠]\s*)+)\n你(\d+)点：((?:[2-9JQKA][♦♣♥♠]\s*)+)"
+    )
+)
 async def blackjack(client: Client, message: Message):
-    zhuang = message.matches[0].group(1)
-    ni_dianshu = message.matches[0].group(2)
-    ni = message.matches[0].group(3)
+    logger.info(message.text)
+    match = message.matches[0]
+    zhuang = match.group(1)
+    ni_dianshu = match.group(2)
+    ni = match.group(3)
     zhuang_cards = zhuang.split()
     ni_cards = ni.split()
     zhuang_score = calculate_score(zhuang_cards)
@@ -42,12 +56,13 @@ async def blackjack(client: Client, message: Message):
         zhuang_simulated = zhuang_cards[:]
         while calculate_score(zhuang_simulated) < 17:
             zhuang_simulated.append(sample(remaining_cards, 1)[0])
-        
+
         zhuang_simulated_score = calculate_score(zhuang_simulated)
-        
+
         if ni_score > zhuang_simulated_score or zhuang_simulated_score > 21:
             win_count += 1
 
     win_probability = win_count / total_simulations
-    await message.reply_text(f"庄的点数: {zhuang_score}\n你的点数: {ni_score}\n你的获胜概率: {win_probability:.2%}")
-    
+    await message.reply_text(
+        f"庄的点数: {zhuang_score}\n你的点数: {ni_score}\n你的获胜概率: {win_probability:.2%}"
+    )
