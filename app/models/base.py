1e0811336c0628e706fee4a55a765d00898dd38d
Yy <yyned2501@vip.qq.com>
2025-02-17 18:43:28 +0800

    bug fix

--- app/models/base.py
+++ app/models/base.py
@@ -5,14 +5,16 @@ from sqlalchemy.ext.asyncio import AsyncAttrs
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    __abstract__ = True
 
 
 class CreateTimeBase(Base):
+    __abstract__ = True
     create_time: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
 
 
 class TimeBase(Base):
+    __abstract__ = True
     create_time: Mapped[datetime.datetime] = mapped_column(DateTime, default=func.now())
     update_time: Mapped[datetime.datetime] = mapped_column(
         DateTime, default=func.now(), onupdate=func.now()
