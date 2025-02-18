4717d26c9038d6a3b58615478310fec4be7b88d9
Yy <yyned2501@vip.qq.com>
2025-02-18 19:53:39 +0800

    优化礼物处理逻辑，调整用户记录获取方式和限制条件

--- app/scripts/ptvicomo/gift.py
+++ app/scripts/ptvicomo/gift.py
@@ -10,40 +10,36 @@ from app.models.transform import Transform, User
 from app.libs.messages import delete_message
 
 TARGET = -1002022762746
+SITE_NAME = "象岛"
 
 
 @app.on_message(
     filters.chat(TARGET) & filters.regex(r"^#小萝莉给点"),
 )
 async def gift(client: Client, message: Message):
-
     today_midnight = datetime.datetime.combine(
         datetime.datetime.today().date(), datetime.datetime.min.time()
-    )
+    ).astimezone(datetime.timezone.utc)
     async with ASSession() as session:
         async with session.begin():
             user = await User.get(message.from_user)
-            async with session.begin_nested():
-                today_tranform = (
-                    (
-                        await session.execute(
-                            select(Transform).filter(
-                                Transform.create_time >= today_midnight,
-                                Transform.site == "象站",
-                                Transform.bonus < 0,
-                            )
+            today_tranform = (
+                (
+                    await session.execute(
+                        select(Transform).filter(
+                            Transform.create_time >= today_midnight,
+                            Transform.site == SITE_NAME,
+                            Transform.bonus < 0,
                         )
                     )
-                    .scalars()
-                    .all()
                 )
-                uids = [tr.user_id for tr in today_tranform]
-                if len(uids) < 50:
-                    if user.id in uids:
-                        delete_message(
-                            await message.reply(f"今天给过了，明天再来！"), 30
-                        )
-                    else:
-                        bonus = randint(100, 1000)
-                        await user.add_transform_record("象站", bonus)
-                        await message.reply(f"+{bonus}")
+                .scalars()
+                .all()
+            )
+            uids = set([tr.user_id for tr in today_tranform])
+            if len(uids) < 10:
+                if user.id in uids:
+                    delete_message(await message.reply(f"今天给过了，明天再来！"), 30)
+                else:
+                    bonus = randint(100, 1000)
+                    await message.reply(f"+{bonus}")
