cf9e212edf099979a2369c10a59b878dba92aa45
Yy <yyned2501@vip.qq.com>
2025-02-19 13:34:28 +0800

    将日志记录替换为打印输出，以简化主模块的调试信息

--- main.py
+++ main.py
@@ -5,22 +5,20 @@ from app import app, scheduler
 from app import models, scripts, schedulers
 from app.scripts.zhuque.ex.bet_modes import create_models
 
-logger = logging.getLogger("main")
-
 
 async def main():
     await app.start()
-    logger.info("app.start()")
+    print("app.start()")
     await models.create_all()
-    logger.info("models.create_all()")
+    print("models.create_all()")
     await create_models()
-    logger.info("create_models()")
+    print("create_models()")
     scheduler.start()
-    logger.info("scheduler.start()")
-    logger.info(scheduler.get_jobs())
-    logger.info("logger.info(scheduler.get_jobs())")
+    print("scheduler.start()")
+    print(scheduler.get_jobs())
+    print("logger.info(scheduler.get_jobs())")
     await idle()
-    logger.info("idle()")
+    print("idle()")
     await app.stop()
 
 
