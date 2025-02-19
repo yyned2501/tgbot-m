d83df937bc8b2e7d903da3162b984a3fe978b474
Yy <yyned2501@vip.qq.com>
2025-02-19 13:49:05 +0800

    优化主程序启动流程，调整日志记录顺序并确保调度器在主程序启动后开始

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ from app import app, scheduler, logger
 from app import models, scripts, schedulers
 from app.scripts.zhuque.ex.bet_modes import create_models
 
-logger.info("主程序启动")
-
 
 async def main():
-    scheduler.start()
-    logger.info("app.start()")
+    logger.info("启动主程序")
     await app.start()
-    logger.info("models.create_all()")
     await models.create_all()
     await create_models()
+    scheduler.start()
     await idle()
     await app.stop()
 
 
 if __name__ == "__main__":
-    logger.info("主程序main启动")
+
     asyncio.run(main())
