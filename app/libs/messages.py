8999295409c08bc7413b16d674a01efe4a3a9f0b
Yy <yyned2501@vip.qq.com>
2025-02-19 10:46:26 +0800

    重构消息删除逻辑，使用Redis存储删除任务信息并定时检查删除时间

--- app/libs/messages.py
+++ app/libs/messages.py
@@ -1,33 +1,52 @@
-import datetime
+import json
 import logging
+import time
 import traceback
 from pyrogram.types.messages_and_media import Message
-from app import scheduler
+from app import app, redis_cli, scheduler
 
 logger = logging.getLogger("main")
 
 
-async def _delete_message(message: Message):
-    logger.info(f"删除消息{message.text}")
-    try:
-        await message.delete()
-    except Exception as e:
-        logger.error(f"Error deleting message: {e}")
-        traceback.print_exc()
-        delete_message(message, 5)
+def delete_message(message: Message, delay: int = 0):
+    data = {
+        "chatid": message.chat.id,
+        "messageid": message.id,
+        "deletetime": int(time.time()) + delay,
+    }
 
-
-def delete_message(message: Message, sleep_sec: int):
-    next_run_time = datetime.datetime.now() + datetime.timedelta(seconds=sleep_sec)
-    logger.info(f"添加定时删除消息{message.text}，删除时间{next_run_time}")
-    scheduler.add_job(
-        _delete_message,
-        "date",
-        next_run_time=next_run_time,
-        args=(message,),
+    redis_cli.set(
+        f"DM:{message.chat.id}:{message.id}",
+        json.dumps(data),
     )
-    jobs = scheduler.get_jobs()
-    for job in jobs:
-        logger.info(
-            f"任务ID: {job.id}, 任务名称: {job.name}, 任务下次运行时间: {job.next_run_time}"
-        )
+
+
+logger = logging.getLogger("test")
+
+
+async def s_delete_message():
+    # 获取所有以 "DM" 开头的键
+    delete_messages_keys = redis_cli.keys("DM*")
+    if delete_messages_keys:
+        for key in delete_messages_keys:
+            value = redis_cli.get(key)
+            value_str: str = value.decode("utf-8")
+            data: dict[str, int] = json.loads(value_str)
+            chatid = data.get("chatid")
+            messageid = data.get("messageid")
+            deletetime = data.get("deletetime")
+            if deletetime:
+                # 检查是否到了删除时间
+                if deletetime <= time.time():
+                    try:
+                        # 尝试删除消息
+                        await app.delete_messages(chatid, messageid)
+                        redis_cli.delete(key)
+                    except Exception as e:
+                        # 捕获异常并记录错误信息
+                        traceback.print_exc()
+                        logger.error(f"Error deleting message: {e}")
+
+
+scheduler.add_job(s_delete_message, "interval", seconds=1)
+logger.info(scheduler.get_jobs)
