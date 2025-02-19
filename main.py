91dc76d3cac705acfde8bd1dfcd84926c1734542
Yy <yyned2501@vip.qq.com>
2025-02-19 13:24:58 +0800

    添加应用启动过程中的日志记录以便于调试

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from app.scripts.zhuque.ex.bet_modes import create_models
 
 async def main():
     await app.start()
+    logger.info("app.start()")
     await models.create_all()
+    logger.info("models.create_all()")
     await create_models()
+    logger.info("create_models()")
     scheduler.start()
+    logger.info("scheduler.start()")
     logger.info(scheduler.get_jobs())
+    logger.info("logger.info(scheduler.get_jobs())")
     await idle()
+    logger.info("idle()")
     await app.stop()
 
 
