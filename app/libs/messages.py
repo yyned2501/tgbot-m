7fd0f3b359048491b5032d06718d8523ecd0f757
Yy <yyned2501@vip.qq.com>
2025-02-19 11:10:30 +0800

    添加日志记录以输出删除任务和待删除消息列表

--- app/libs/messages.py
+++ app/libs/messages.py
@@ -9,16 +9,18 @@ logger = logging.getLogger("main")
 
 
 def delete_message(message: Message, delay: int = 0):
+    logger.info(f"添加删除任务DM:{message.chat.id}:{message.id}")
     data = {
         "chatid": message.chat.id,
         "messageid": message.id,
         "deletetime": int(time.time()) + delay,
     }
-
     redis_cli.set(
         f"DM:{message.chat.id}:{message.id}",
         json.dumps(data),
     )
+    delete_messages_keys = redis_cli.keys("DM*")
+    logger.info(f"待删除的消息列表{delete_messages_keys}")
 
 
 async def s_delete_message():
