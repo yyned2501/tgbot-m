b7f0766980d80d584278d3d131406d85afa3ecc4
Yy <yyned2501@vip.qq.com>
2025-02-19 13:14:41 +0800

    优化定时任务加载逻辑，移除冗余日志并添加作业下次运行时间的日志记录

--- app/schedulers/__init__.py
+++ app/schedulers/__init__.py
@@ -1,10 +1,7 @@
 import importlib
 
 from app.config import launch
-from app import logger
-logger.info("加载定时任务")
-logger.info(__name__.split(".")[-1])
+
 if launch.get(__name__.split(".")[-1]):
     for sites in launch.get(__name__.split(".")[-1]):
-        logger.info(f"{__name__}.{sites}")
         importlib.import_module(f"{__name__}.{sites}")
