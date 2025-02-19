c934141e3c74115d3b1502f6c325c470dda533fa
Yy <yyned2501@vip.qq.com>
2025-02-19 13:37:27 +0800

    添加主程序启动和运行过程中的日志记录以便于调试

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-import logging
 from pyrogram import idle
 
-from app import app, scheduler
+from app import app, scheduler, logger
 from app import models, scripts, schedulers
 from app.scripts.zhuque.ex.bet_modes import create_models
+
+logger.info("主程序启动")
 
 
 async def main():
     await app.start()
-    print("app.start()")
     await models.create_all()
-    print("models.create_all()")
     await create_models()
-    print("create_models()")
     scheduler.start()
-    print("scheduler.start()")
-    print(scheduler.get_jobs())
-    print("logger.info(scheduler.get_jobs())")
     await idle()
-    print("idle()")
     await app.stop()
 
 
 if __name__ == "__main__":
+    logger.info("主程序main启动")
     app.run(main())
