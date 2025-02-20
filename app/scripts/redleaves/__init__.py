0770e834595b26a37e156e7a199ca889b2e7b3fe
Yy <yyned2501@vip.qq.com>
2025-02-20 15:50:06 +0800

    删除不再需要的模块，简化代码结构

--- app/scripts/redleaves/__init__.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-from app.config import launch
-
-method = __name__.split(".")[-2]
-site = __name__.split(".")[-1]
-for task in launch.get(method).get(site):
-    importlib.import_module(f"{__name__}.{task}")
