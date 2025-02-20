b4a235f08cc224c5661c01bd8845fe9853415009
Yy <yyned2501@vip.qq.com>
2025-02-20 16:53:31 +0800

    重构主程序启动逻辑，简化代码结构并引入异步启动函数

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
-import asyncio
 import os
 
 import redis
-from pyrogram import Client
+from pyrogram import Client as _Client
+from pyrogram import idle
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from app.libs.logs import logger
 from app.libs.async_token_bucket import AsyncTokenBucket
+from app import models
 from app.config import setting
+from app.scripts.zhuque.ex.bet_modes import create_models
 
 
-class MyClient(Client):
+class Client(_Client):
     def __init__(self, *arg, **karg):
         super().__init__(*arg, **karg)
         self.bucket = AsyncTokenBucket(capacity=10, fill_rate=1)
@@ -18,14 +21,6 @@ class MyClient(Client):
     async def invoke(self, *arg, **kargs):
         await self.bucket.consume()
         return await super().invoke(*arg, **kargs)
-
-    def run(self, *arg, **kargs):
-        if len(arg) > 0:
-            co = arg[0]
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(co)
-        else:
-            super().run(*arg, **kargs)
 
 
 os.environ["TZ"] = "Asia/Shanghai"
@@ -44,14 +39,30 @@ else:
     proxy = None
 
 
-# app = MyClient(
-#     "sessions/tgbot",
-#     api_id=setting["tg"]["api_id"],
-#     api_hash=setting["tg"]["api_hash"],
-#     proxy=proxy,
-# )
 redis_cli = redis.Redis(
     host=setting["redis"]["host"],
     port=setting["redis"]["port"],
     db=setting["redis"]["db"],
 )
+import asyncio
+
+from app import Client, scheduler, logger, setting, proxy
+
+
+async def start_app():
+    app = Client(
+        "sessions/tgbot",
+        api_id=setting["tg"]["api_id"],
+        api_hash=setting["tg"]["api_hash"],
+        proxy=proxy,
+        plugins=dict(root="scripts", include=["zhuque"]),
+    )
+    logger.info("启动主程序")
+    await app.start()
+    await models.create_all()
+    await create_models()
+    scheduler.start()
+    logger.info("监听主程序")
+    await idle()
+    await app.stop()
+    logger.info("关闭主程序")
--- main.py
+++ main.py
@@ -6,32 +6,7 @@ if sys.platform != "win32":
     uvloop.install()
 
 import asyncio
-
-from pyrogram import idle
-from app import Client, scheduler, logger, setting, proxy
-from app import models
-
-from app.scripts.zhuque.ex.bet_modes import create_models
-
-
-async def main():
-    app = Client(
-        "sessions/tgbot",
-        api_id=setting["tg"]["api_id"],
-        api_hash=setting["tg"]["api_hash"],
-        proxy=proxy,
-        plugins=dict(root="app.scripts", include=["zhuque"]),
-    )
-    logger.info("启动主程序")
-    await app.start()
-    await models.create_all()
-    await create_models()
-    scheduler.start()
-    logger.info("监听主程序")
-    await idle()
-    await app.stop()
-    logger.info("关闭主程序")
-
+from app import start_app
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_app())
