ca171d0f5e80f422bcef67bb6c2d13a6ec716544
Yy <yyned2501@vip.qq.com>
2025-02-20 15:33:14 +0800

    删除不再需要的动态导入逻辑，简化脚本结构

--- app/scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-import importlib
-
-from app.config import launch
-
-if launch.get(__name__.split(".")[-1]):
-    for sites in launch.get(__name__.split(".")[-1]):
-        importlib.import_module(f"{__name__}.{sites}")
