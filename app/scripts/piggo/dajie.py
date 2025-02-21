45db906d7119f6849de83f9d90b7692a6bf9a00f
Yy <yyned2501@vip.qq.com>
2025-02-21 15:44:36 +0800

    添加猪猪打劫脚本，监控被打劫

---
+++ app/scripts/piggo/dajie.py
@@ -0,0 +1,41 @@
+import asyncio
+from random import randint
+import re
+from pyrogram import filters
+from pyrogram.enums import ParseMode
+from pyrogram.types.messages_and_media import Message
+
+from app import Client, logger
+from app.filters import custom_filters
+
+GROUP = -1002022762746
+BOT = 6121385204
+
+
+@Client.on_message(
+    filters.chat(GROUP)
+    & custom_filters.create_bot_filter(BOT)
+    & custom_filters.command_to_me
+    & filters.regex(r"真遗憾.*获得\s+([\d\.]+)\s+个魔力")
+)
+async def dajie_win(client: Client, message: Message):
+    match = message.matches[0]
+    bonus = match.group(1)
+    await message.reply_to_message.reply(f"感谢大佬赠送的 {bonus} 魔力，爱你哟")
+
+
+@Client.on_message(
+    filters.chat(GROUP)
+    & custom_filters.create_bot_filter(BOT)
+    & custom_filters.command_to_me
+    & filters.regex(
+        r"你成功打劫了.*加倍羞辱了 (\d+) 次 扣除税 (\d+) 共获得 (\d+) 个魔力"
+    )
+)
+async def dajie_win(client: Client, message: Message):
+    match = message.matches[0]
+    count = match.group(1)
+    tax = match.group(2)
+    bonus = match.group(3)
+    await message.reply_to_message.reply(f"竟敢抢我魔力 {bonus+tax} ? 看我打劫回来！")
+    await message.reply_to_message.reply(f"/dajie {count}")
