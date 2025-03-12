ade8ff6bac87d080d06217e8122e0ac12c8ea325
Yy <yyned2501@vip.qq.com>
2025-03-12 19:08:11 +0800

    bug fix

--- app/scripts/zhuque/dajie.py
+++ app/scripts/zhuque/dajie.py
@@ -10,7 +10,7 @@ TARGET = -1001833464786
 @Client.on_message(filters.chat(TARGET) & filters.reply & filters.command("fdajie"))
 async def fdajie(client: Client, message: Message):
     count = int(message.command[1])
-    if len(message.command) == 2:
+    if len(message.command) > 2:
         new_name = message.command[2]
     first_name = client.me.first_name
     last_name = client.me.last_name
