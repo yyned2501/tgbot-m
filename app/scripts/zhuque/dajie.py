8dc2ce85472681fdf3fe8ef46e3e5f15ecbd328f
Yy <yyned2501@vip.qq.com>
2025-03-12 19:12:15 +0800

    更快的删除

--- app/scripts/zhuque/dajie.py
+++ app/scripts/zhuque/dajie.py
@@ -9,13 +9,13 @@ TARGET = -1001833464786
 
 @Client.on_message(filters.chat(TARGET) & filters.reply & filters.command("fdajie"))
 async def fdajie(client: Client, message: Message):
+    await message.delete()
     count = int(message.command[1])
     if len(message.command) > 2:
         new_name = message.command[2]
     first_name = client.me.first_name
     last_name = client.me.last_name
     await client.update_profile(new_name)
-    return_message = await message.reply_to_message.reply(f"/dajie {count}")
-    await message.delete()
-    await return_message.delete()
+    await (await message.reply_to_message.reply(f"/dajie {count}")).delete()
     await client.update_profile(first_name, last_name)
+    
