3b815e5cf1b8539f35369fa5398c75cb8d8b9c72
Yy <yyned2501@vip.qq.com>
2025-03-12 21:05:47 +0800

    bug fix

--- app/scripts/zhuque/dajie.py
+++ app/scripts/zhuque/dajie.py
@@ -10,7 +10,7 @@ TARGET = -1001833464786
 
 @Client.on_message(filters.chat(TARGET) & filters.reply & filters.command("fdajie"))
 async def fdajie(client: Client, message: Message):
-    await message.delete()
+
     count = int(message.command[1])
     if len(message.command) > 2:
         new_first_name = message.command[2]
@@ -20,8 +20,12 @@ async def fdajie(client: Client, message: Message):
         new_last_name = message.reply_to_message.from_user.last_name
     first_name = client.me.first_name
     last_name = client.me.last_name
-    await client.update_profile(new_first_name, new_last_name)
-    r_message = await message.reply_to_message.reply(f"/dajie {count}")
-    await asyncio.sleep(1)
-    await r_message.delete()
-    await client.update_profile(first_name, last_name)
+    try:
+        await client.update_profile(new_first_name, new_last_name)
+        r_message = await message.reply_to_message.reply(f"/dajie {count}")
+        await message.delete()
+        await asyncio.sleep(1)
+        await r_message.delete()
+    finally:
+        await message.delete()
+        await client.update_profile(first_name, last_name)
