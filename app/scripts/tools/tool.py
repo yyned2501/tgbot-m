448820649c79c2e8422ed8c54a0a3a4273320766
Yy <yyned2501@vip.qq.com>
2025-03-21 20:09:01 +0800

    test

--- app/scripts/tools/tool.py
+++ app/scripts/tools/tool.py
@@ -73,7 +73,8 @@ async def self_delatemessage(client: Client, message: Message):
 async def call_self_delatemessage(client: Client, message: Message):
     await self_delatemessage(client, message)
 
-@Client.on_message(filters.me & filters.command("lphoto") )
+
+@Client.on_message(filters.me & filters.command("lphoto"))
 async def lphoto(client: Client, message: Message):
     photos_list = []
     async for photo in client.get_chat_photos("me"):
@@ -84,6 +85,8 @@ async def lphoto(client: Client, message: Message):
         await client.send_document("me", photo)
 
     await message.edit("所有头像已发送到你的私聊。")
+
+
 @Client.on_message(filters.me & filters.command("gphoto") & filters.reply)
 async def gphoto(client: Client, message: Message):
     photos_list = []
@@ -105,6 +108,10 @@ async def sphoto(client: Client, message: Message):
 
 @Client.on_message(filters.me & filters.command("dphoto"))
 async def dphoto(client: Client, message: Message):
-    async for photo in client.get_chat_photos("me", limit=1):
+    n = 0
+    async for photo in client.get_chat_photos("me"):
+        if n == 0:
+            n += 1
+            continue
         await client.delete_profile_photos(photo.file_id)
     await message.delete()
--- app/scripts/zhuque/dajie.py
+++ app/scripts/zhuque/dajie.py
@@ -3,9 +3,11 @@ from pyrogram import filters
 from pyrogram.types.messages_and_media import Message
 
 from app import Client
+import os
 
 
 TARGET = -1001833464786
+BOT_ID = 5697370563
 
 
 @Client.on_message(
@@ -21,12 +23,19 @@ async def fdajie(client: Client, message: Message):
         new_last_name = message.reply_to_message.from_user.last_name
     reply_message_id = message.reply_to_message.id
     await message.delete()
+    if not os.path.exists("downloads/photo.jpg"):  # 下载头像
+        async for photo in client.get_chat_photos(BOT_ID, 1):
+            await client.download_media(photo.file_id, file_name="photo.jpg")
     first_name = client.me.first_name
     last_name = client.me.last_name
-    await client.update_profile(new_first_name, new_last_name)
+    await client.update_profile(new_first_name, new_last_name)  # 更新名字
+    await client.set_profile_photo(photo="downloads/photo.jpg")  # 更新头像
     r_message = await client.send_message(
         TARGET, f"/dajie {count}", reply_to_message_id=reply_message_id
     )
+
     await asyncio.sleep(1)
     await r_message.delete()
-    await client.update_profile(first_name, last_name)
+    await client.update_profile(first_name, last_name)  # 恢复名字
+    async for photo in client.get_chat_photos("me", 1):
+        await client.delete_profile_photos(photo.file_id)  # 删除头像
